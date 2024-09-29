5a3822f465825b870dc857e98bc39ffd9039be22
David Jiang <jiangguanlin2003@outlook.com>
2024-09-29 23:05:49 +0800

    feat & fix: README & routers

---
+++ README.md
@@ -0,0 +1,18 @@
+# Trading Engine Web API
+
+Stock & Crypto Trading Engine Web API written by Rust
+
+## Tech Stack
+
+- Language: Rust
+- Web Framework: Rocket
+
+## Function
+
+- Stock Trading market / limit order
+- Crypto Trading market / limit order
+- Bid / Ask Pricing Matching Engine
+- Bid / Ask Order book
+- Stock / Crypto Market trading symbol / pair
+- Bid / Ask quote for Stock / Crypto Market
+
--- src/routers/orders.rs
+++ src/routers/orders.rs
@@ -6,10 +6,20 @@ use rust_decimal::Decimal;
 // use rust_decimal_macros::dec;
 
 #[derive(Deserialize)]
-pub struct OrderRequest {
+pub struct CryptoOrderRequest {
     pub order_type: String,
     pub price: f64,
     pub amount: Decimal,
+    pub trading_pair_base: String,
+    pub trading_pair_quote: String,
+}
+
+#[derive(Deserialize)]
+pub struct StockOrderRequest {
+    pub order_type: String,
+    pub price: f64,
+    pub amount: Decimal,
+    pub trading_symbol: String,
 }
 
 #[derive(Serialize)]
@@ -17,37 +27,41 @@ pub struct Response {
     pub message: String,
 }
 
-#[post("/crypto_order", format = "json", data = "<order_request>")]
-pub async fn crypto_order_route(order_request: Json<OrderRequest>) -> Json<Response> {
+#[post("/crypto_order", format = "json", data = "<crypto_order_request>")]
+pub async fn crypto_order_route(crypto_order_request: Json<CryptoOrderRequest>) -> Json<Response> {
     let mut engine = CryptoMatchingEngine::new();
-    let pair = CryptoTradingPair::new("BTC".to_string(), "USD".to_string());
+    let pair = CryptoTradingPair::new(
+        crypto_order_request.trading_pair_base.to_uppercase().to_string(), 
+        crypto_order_request.trading_pair_quote.to_uppercase().to_string()
+    );
+
     engine.add_new_market(pair.clone());
 
-    let order = Order::new(match order_request.order_type.as_str() {
+    let order = Order::new(match crypto_order_request.order_type.as_str() {
         "buy" => BidOrAsk::Bid,
         "sell" => BidOrAsk::Ask,
         _ => return Json(Response { message: "Invalid order type".to_string() }),
-    }, order_request.price);
+    }, crypto_order_request.price);
 
-    match engine.place_limit_order(pair, order_request.amount, order) {
+    match engine.place_limit_order(pair, crypto_order_request.amount, order) {
         Ok(_) => Json(Response { message: "Crypto order placed successfully".to_string() }),
         Err(e) => Json(Response { message: format!("Error placing order: {}", e) }),
     }
 }
 
-#[post("/stock_order", format = "json", data = "<order_request>")]
-pub async fn stock_order_route(order_request: Json<OrderRequest>) -> Json<Response> {
+#[post("/stock_order", format = "json", data = "<stock_order_request>")]
+pub async fn stock_order_route(stock_order_request: Json<StockOrderRequest>) -> Json<Response> {
     let mut stock_engine = StockMatchingEngine::new();
-    let symbol = StockTradingSymbol::new("AAPL".to_string());
+    let symbol = StockTradingSymbol::new(stock_order_request.trading_symbol.to_uppercase().to_string());
     stock_engine.add_new_market(symbol.clone());
 
-    let order = Order::new(match order_request.order_type.as_str() {
+    let order = Order::new(match stock_order_request.order_type.as_str() {
         "buy" => BidOrAsk::Bid,
         "sell" => BidOrAsk::Ask,
         _ => return Json(Response { message: "Invalid order type".to_string() }),
-    }, order_request.price);
+    }, stock_order_request.price);
 
-    match stock_engine.place_limit_order(symbol, order_request.amount, order) {
+    match stock_engine.place_limit_order(symbol, stock_order_request.amount, order) {
         Ok(_) => Json(Response { message: "Stock order placed successfully".to_string() }),
         Err(e) => Json(Response { message: format!("Error placing order: {}", e) }),
     }
